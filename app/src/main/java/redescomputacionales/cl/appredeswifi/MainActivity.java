1a78ea3930f1774d9d25ed1baac7d3fd65c7f36a
Sebastián Pinto-Agüero González <sebastian.pintoaguero@usach.cl>
2018-05-14 20:10:17 -0400

    Map implemented, Google Maps API Key connected, GPS location and manual location implemented. Fixed.

--- app/src/main/java/redescomputacionales/cl/appredeswifi/MainActivity.java
+++ app/src/main/java/redescomputacionales/cl/appredeswifi/MainActivity.java
@@ -1,8 +1,15 @@
 package redescomputacionales.cl.appredeswifi;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +19,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener, GoogleMap.OnMarkerDragListener {
+
+    //Codigo para pedir permiso
+    static final Integer GPS_SETTINGS = 0x7;
+
+    //Se agrega un MapFragment y un GoogleMap
+    private SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+
+    //Obtiene la posición actual
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    //Posición GPS del dispositivo
+    private LatLng gpsLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +75,38 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Soporte para el MapFragment
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map); //R.id.map es un fragmento que está en content_main.xml
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+        //Para obtener la última (o actual) ubicacion conocida
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+
+        //Permisos
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        GPS_SETTINGS);
+            }
+        }
+
+
+
     }
 
     @Override
@@ -97,5 +164,92 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+        getCurrentLocation();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        mMap.clear();
+        getCurrentLocation();
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        gpsLocation = marker.getPosition();
+        Toast.makeText(this, "Posición final: " + gpsLocation.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void updateGpsLocation(Location location)
+    {
+        //Asigna la LatLng a partir la ubicación GPS
+        gpsLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        Toast.makeText(this, "Posición actual: " + gpsLocation.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void addMarkerToLocation(Location location, String tittle)
+    {
+        //Añade un marcador en el mapa
+        mMap.addMarker(new MarkerOptions()
+                .position(gpsLocation)
+                .title(tittle)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .draggable(true)
+        );
+    }
+
+    public void moveCameraToLocation(Location location)
+    {
+        //Mueve el mapa con movimiento suave
+        CameraUpdate camara = CameraUpdateFactory.newLatLngZoom(
+                gpsLocation, 18);
+        mMap.animateCamera(camara);
+    }
+
+    @SuppressLint("MissingPermission")
+    public void getCurrentLocation()
+    //Obtiene la posición actual y añade un marcador en el mapa
+    {
+        //Obtiene la posicion actual
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            updateGpsLocation(location);
+                            addMarkerToLocation(location, "Posición Actual");
+                            moveCameraToLocation(location);
+                        }
+                    }
+                });
     }
 }
